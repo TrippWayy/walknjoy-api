f61bf930382205196e0841208c477f78c14c2443
gt-nm-1711 <nijat.mansimov_contractor@gaintheory.com>
2023-09-13 00:09:08 +0400

    Created auth, hotel, room, user API also models also error middlewares

---
+++ src/routes/hotels.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotelRooms,
+  getHotels,
+  updateHotel,
+} = require("../controllers/hotel.js");
+import {verifyAdmin} from "../utils/verifyToken.js"
+
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+//GET
+router.get("/find/:id", getHotel);
+//GET ALL
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
+
+module.exports = router;
---
+++ src/routes/rooms.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+  updateRoomAvailability,
+} = require("../controllers/room");
+const { verifyAdmin } = require("../utils/verifyToken");
+
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+//UPDATE
+router.put("/availability/:id", updateRoomAvailability);
+router.put("/:id", verifyAdmin, updateRoom);
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+//GET
+router.get("/:id", getRoom);
+//GET ALL
+router.get("/", getRooms);
+
+module.exports = router;
