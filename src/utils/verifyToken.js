36ecc4b9be3381048c535576ae446679f85e4e19
gt-nm-1711 <nijat.mansimov_contractor@gaintheory.com>
2023-10-02 23:36:25 +0400

    added check logon

--- src/utils/verifyToken.js
+++ src/utils/verifyToken.js
@@ -1,5 +1,5 @@
 const createError = require("../utils/error")
-
+const User = require("../model/User")
 const checkLogin = (req, res, next)=>{
   if(req.isAuthenticated()){
     return next()
@@ -37,4 +37,17 @@ const checkAdmin = (req, res, next)=>{
   }
 }
 
-module.exports = {checkLogin, checkUnLogin, checkAdmin}
+const checkAdminLogin = async (req, res, next)=>{
+  if(req.body.username){
+    const user = await User.findOne({username: username})
+    if(user.isAdmin){
+      return next()
+    }else{
+      return next(createError(400, "You are not allowed!"))
+    }
+  }else{
+    return next(createError(400, "Username field should be fulled!"))
+  }
+}
+
+module.exports = {checkLogin, checkUnLogin, checkAdmin, checkAdminLogin}
