69ecb333cdc12f7b76f57f3297e5855fe9f6ca28
gt-nm-1711 <nijat.mansimov_contractor@gaintheory.com>
2023-10-14 21:17:46 +0400

    Fixed get hotels query bug

--- src/controllers/getCountByCity.js
+++ src/controllers/getCountByCity.js
@@ -5,81 +5,31 @@ const Car = require("../model/Car");
 const countByCity = async (req, res, next) => {
   try {
     const regionsOfAzerbaijan = [
-  "Baku",
-  "Ganja",
-  "Sumgait",
-  "Mingachevir",
-  "Lankaran",
-  "Shirvan",
-  "Sheki",
-  "Nakhchivan",
-  "Goychay",
-  "Quba",
-  "Shamakhi",
-  "Sabirabad",
-  "Khachmaz",
-  "Goygol",
-  "Agdam",
-  "Agdash",
-  "Agjabadi",
-  "Astara",
-  "Balakan",
-  "Beylagan",
-  "Bilasuvar",
-  "Dashkasan",
-  "Fizuli",
-  "Gadabay",
-  "Goranboy",
-  "Goygol",
-  "Hajigabul",
-  "Imishli",
-  "Ismailli",
-  "Jalilabad",
-  "Kurdamir",
-  "Lankaran",
-  "Lerik",
-  "Masally",
-  "Neftchala",
-  "Oghuz",
-  "Ordubad",
-  "Qabala",
-  "Qakh",
-  "Qazakh",
-  "Quba",
-  "Qubadli",
-  "Shamakhi",
-  "Shaki",
-  "Shamkir",
-  "Shusha",
-  "Siazan",
-  "Tartar",
-  "Tovuz",
-  "Ujar",
-  "Yardymli",
-  "Zaqatala",
-  "Zardab"
-];
+      "Baku",
+      "Ganja",
+      "Lankaran",
+      "Ismayilli",
+      "Gabala",
+      "Oguz"
+    ];
 
-    const countPromises = regionsOfAzerbaijan.map(async (city) => ({
-      city,
-      hotels: Hotel.countDocuments({ city }).lean().exec(), // Execute the query
-      tours: Tour.countDocuments({ city }).lean().exec(), // Execute the query
-      cars: Car.countDocuments({ city }).lean().exec(), // Execute the query
-    }));
+    const countPromises = regionsOfAzerbaijan.map(async (city) => {
+      const [hotelCount, tourCount, carCount] = await Promise.all([
+        Hotel.countDocuments({ city }).lean().exec(),
+        Tour.countDocuments({ city }).lean().exec(),
+        Car.countDocuments({ city }).lean().exec()
+      ]);
+
+      return { city, hotels: hotelCount, tours: tourCount, cars: carCount };
+    });
 
     const counts = await Promise.all(countPromises);
-
-    // Wait for all the query executions to complete
-    await Promise.all(counts.map(async (countObject) => {
-      countObject.hotels = await countObject.hotels;
-      countObject.tours = await countObject.tours;
-      countObject.cars = await countObject.cars;
-    }));
 
     res.status(200).json({ counts });
   } catch (err) {
     next(err);
   }
 };
+
 
 module.exports = countByCity;
