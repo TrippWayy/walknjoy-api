065ad5952e4fd71979ea3c2258e9b9bbcba4bc0c
TrippWayy <144560354+TrippWayy@users.noreply.github.com>
2023-12-10 00:57:51 +0400

    Update blog.js

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -2,9 +2,6 @@ const router = require("express").Router()
 const {getBlogs, getBlog} = require("../controllers/blog")
 const {checkAdmin, checkLogin} = require("../utils/verifyToken")
 const {addReview, getReviews} = require("../controllers/blog");
-const {cronNews} = require("../middlewares/cronMiddleware")
-const {uploadBlog} = require("../middlewares/multer");
-const {blogCloud} = require("../middlewares/cloudinary");
 
 // GET
 router.get("/", getBlogs)
